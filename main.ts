d993a553115c223e5a1d2957ba239ff56e527739
Tácio Franco <98967235+Tryndamoron@users.noreply.github.com>
2022-08-05 16:32:02 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,16 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-    if (Global == 0) {
-        Global += 1
-        Número1 += -1
-        basic.showNumber(Número1)
-    } else {
-        Número1 += -1
-        basic.showNumber(Número1)
+    if (Operação == 1) {
+        Global = Número1
+        Número1 = 0
+        Operação = 2
     }
+    Número1 = Número1 - 1
+    basic.showNumber(Número1)
 })
 input.onButtonPressed(Button.AB, function () {
-    if (Global < 2) {
-        Global += 1
+    if (Operação == 0) {
+        Operação = 1
+    }
+    if (Operação == 1) {
         Operador += 1
         if (Operador == 1) {
             basic.showLeds(`
@@ -70,33 +71,37 @@ input.onButtonPressed(Button.AB, function () {
                 }
             }
         }
-    } else {
-        if (Global >= 2) {
-            if (Operador == 1) {
-                basic.showNumber(Número1 + Número1)
+    }
+    if (Operação == 2) {
+        if (Operador == 1) {
+            basic.showNumber(Global + Número1)
+        } else {
+            if (Operador == 2) {
+                basic.showNumber(Global - Número1)
             } else {
-                if (Operador == 2) {
-                    basic.showNumber(Número1 - Número1)
+                if (Operador == 3) {
+                    basic.showNumber(Global * Número1)
                 } else {
-                    if (Operador == 3) {
-                        basic.showNumber(Número1 * Número1)
-                    } else {
-                        basic.showNumber(Número1 / Número1)
-                    }
+                    basic.showNumber(Global / Número1)
                 }
             }
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
-    if (Global < 2) {
-        Número1 += 1
-        basic.showNumber(Número1)
+    if (Operação == 1) {
+        Global = Número1
+        Número1 = 0
+        Operação = 2
     }
+    Número1 = Número1 + 1
+    basic.showNumber(Número1)
 })
 let Global = 0
 let Número1 = 0
 let Operador = 0
+let Operação = 0
+Operação = 0
 Operador = 0
 Número1 = 0
 Global = 0
